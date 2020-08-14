e2d156a9e0a37bbd94b051e98e8da326ccacc2e9
Clementine Urquizar <clementine@meilisearch.com>
2020-08-15 01:05:19 +0200

    Add PHP in tests of this repo

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+ret_code=0
 temp_file='temp_file.yml'
 ruby_config_file='./examples/config/ruby-config.yml'
 ruby_expected_output='./tests/expected-output-ruby.yml'
+php_config_file='./examples/config/php-config.yml'
+php_expected_output='./tests/expected-output-php.yml'
 
-bundle exec ruby src/main.rb "$ruby_config_file" > $temp_file 2> /dev/null
-diff "$ruby_expected_output" "$temp_file" &> /dev/null
-if [[ "$?" -eq 0 ]]; then
-    echo "OK"
-    rm -f "$temp_file"
-    exit 0
-else
-    echo "FAIL!"
-    echo 'Diff found:'
-    diff "$ruby_expected_output" "$temp_file"
-    rm -f "$temp_file"
-    exit 1
-fi
+# Usage: test_with $language $config_file $output_file
+test_with() {
+  echo "Testing with $1..."
+  bundle exec ruby src/main.rb "$2" > $temp_file 2> /dev/null
+  diff "$3" "$temp_file" &> /dev/null
+  if [[ "$?" -eq 0 ]]; then
+      echo "OK"
+  else
+      echo "FAIL!"
+      echo 'Diff found:'
+      diff "$3" "$temp_file"
+      ret_code=1
+  fi
+}
+
+test_with 'ruby' "$ruby_config_file" "$ruby_expected_output"
+test_with 'PHP' "$php_config_file" "$php_expected_output"
+rm -f "$temp_file"
+exit $ret_code
